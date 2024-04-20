53c7edd70e470144fa88b029ea662dbe4cdc3a38
Sonamuhan <4698516@naver.com>
2024-04-20 09:34:34 +0900

    4/20

---
+++ star-rating/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// StarRating 컴포넌트를 import 한다
+import StarRating from './components/star-rating';
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating />
+    </div>  
+  );
+}
+
+export default App;
---
+++ star-rating/src/components/star-rating/index.jsx
@@ -0,0 +1,15 @@
+// 컴포넌트를 만들어서 내보낸다 export
+
+// 리액트 아이콘 설치
+// npm install react-icons
+
+// StarRating 컴포넌트
+export default function StarRating() {
+  return (
+    <>
+      <div>
+        안녕하세요
+      </div>
+    </>
+  )
+}
