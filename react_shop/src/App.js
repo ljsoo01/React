12d17347bf866ab3f70572c0a8647b1a7f6f4780
ljsoo01 <ljsu1225@gmail.com>
2024-04-28 12:43:42 +0900

    4/28

--- react_shop/src/App.js
+++ react_shop/src/App.js
@@ -1,27 +1,28 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Nav, Navbar, Button, Row, Col} from 'react-bootstrap';
+import {Container, Col, Row, Nav, Navbar, Button} from 'react-bootstrap'
 import './App.css';
-import { useState } from 'react';
-import {Routes, Route, useNavigate} from 'react-router-dom';
+import {Routes, Route, useNavigate, Outlet, Link} from 'react-router-dom';
 import DetailPage from './pages/Detail.js';
-// css를 제공해주는 사이트: bootstrap
+// css를 제공해주는 사이트 : bootstrap
 // npm install react-bootstrap bootstrap
 
 // 자바스크립트 파일은 확장자명을 생략
-// 다른 자바스크립트에서 export한건 import로 가져와서 사용 (변수처럼 사용)
+// 다른 자바스크립트에서 export 한건 import로 가져와서 사용 (변수처럼 사용)
 import data from './data'
 import {num1, num2, num3} from './data.js'
 
 // 이미지를 사용하려면 import
-import mainBG from './main.png'; // 이미지 import
-
+import main from './main.png';      // 이미지 import
+import { useState } from 'react';
+import Cart from './pages/Cart.js';
+// import Cart from './pages/Cart.js';
 
 // 라우터는 창을 새로 불러오는게 아니라 재렌더링 방식을 사용
 function App() {
-  let [items, setItems] = useState(data); // data는 변수니깐 state로
-  let [photo, setPhoto] = useState(['/camera.png','/lens.png','/films.png']); // useState import
-  let navigate = useNavigate();
+  let [items, setItems] = useState(data);   // data는 변수니깐 state로 (data => items)
+  let [photo, setPhoto] = useState(['/camera.png','/lens.png','/films.png', '/logo192.png']);   // useState import
+  let navigate = useNavigate()
 
   return (
     <div className="App">
@@ -29,11 +30,12 @@ function App() {
         <Container>
           <Navbar.Brand href="/"><img src={'/logo192.png'} width={'50px'} height={'50px'}/></Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link onClick={() => {navigate('/')}}>홈</Nav.Link>
-            <Nav.Link onClick={() => {navigate('/detail')}}>상세페이지</Nav.Link>
-            <Nav.Link onClick={() => {navigate('/about')}}>About</Nav.Link>
-            <Nav.Link onClick={() => {navigate(-1)}}>뒤로가기</Nav.Link>
-            <Nav.Link onClick={() => {navigate(1)}}>앞으로가기</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>홈</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail/0')}}>상세페이지</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/about')}}>About</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/cart')}}>장바구니</Nav.Link>
+            <Nav.Link onClick={()=>{navigate(-1)}}>뒤로가기</Nav.Link>
+            <Nav.Link onClick={()=>{navigate(1)}}>앞으로가기</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -41,77 +43,76 @@ function App() {
       <Routes>
         <Route path='/' element={
           <>
-          <div className={'main-bg'}></div>
+            <div className={'main-bg'}></div>
             <Container>
               <Row>
-                {/* data갯수와 image가 바뀔 수 있으니까 useState 처리 
-                  data: 변수 (사용자 입력 또는 서버로 부터 데이터를 받았을때 변경이 되어도 화면은 안바뀜 ==> useState로 변경)
-                */}
-                {/* map을 통해서 useState(data) 만큼 반복 */}
-                {/* <ItemCol data={data[0]} img={photo[0]}/>
-                <ItemCol data={data[1]} img={photo[1]}/>
-                <ItemCol data={data[2]} img={photo[2]}/> */}
-                {items.map((item, idx) => (
-                  <ItemCol key={idx} data={item} img={photo[idx]} />
-                ))}
+                {
+                  items.map((e, idx)=>{
+                    return(
+                      <ItemCol data={e} img={photo[idx]} key={e}/>
+                    )
+                  })
+                }
               </Row>
             </Container>
           </>
         }></Route>
-        <Route path='/detail' element={<DetailPage items={items}/>}></Route>
+        {/* 
+          :id   ==> URL파라미터(변수)를 통해서 상세아이템 변경
+        */}
+        <Route path='/detail/:id' element={<DetailPage items={items} img={photo}/>}></Route>
+        {/*
+        <Route path='/detail/1' element={<DetailPage items={items} id={1} img={photo}/>}></Route>
+        <Route path='/detail/2' element={<DetailPage items={items} id={2} img={photo}/>}></Route> 
+        */}
+        <Route path='/about' element={<AboutPage/>}>
+          <Route path='address' element={<div>주소</div>}></Route>
+          <Route path='location' element={<div>위치</div>}></Route>
+        </Route>
+        <Route path='/cart' element={<Cart />}></Route>
+        <Route path='*' element={<div>그 외의 페이지(404)</div>}></Route>
       </Routes>
 
+      {/* 
+        리액트는 하나의 html을 다시 그리는 방식이기 때문에
+        html을 이동하는 <a>태그 보다는 리액트 라우터의 <Link>를 사용
+      <Link to="/about/address"><Button variant="warning">리액트 부트스트랩 버튼</Button></Link>
+      <a href="/about/location">a태그로 이동</a>
+      */}
 
-      {/* <Container>
-        <Row>
-          <Col>
-            <img src={'/camera.png'} width={'200px'} height={'200px'} />
-            <h4>{data[0].title}</h4>
-            <p>{data[0].price}원</p>
-          </Col>
-          <Col>
-            <img src={'/lens.png'} width={'200px'} height={'200px'} />
-            <h4>{data[1].title}</h4>
-            <p>{data[1].price}원</p>
-          </Col>
-          <Col>
-            <img src={'/films.png'} width={'200px'} height={'200px'} />
-            <h4>{data[2].title}</h4>
-            <p>{data[2].price}원</p>
-          </Col>
-        </Row>
-      </Container> */}
-
-    {/* 
-      <div style={{backgroundImage: `url(/logo192.png)`,
-      height:'300px', width:'100%',
-      backgroundSize:'cover',
-      backgroundPosition:'center'}}>
-
-      </div> */}
-
-
-      {/* <br/>
-      <Button variant="primary">기본버튼</Button>{' '} */}
     </div>
   );
 }
 
 export default App;
 
-
 // 컴포넌트는 맨 앞글자는 대문자
-// return에 html(JSX) 코드
+// return에 html(jsx) 코드
 // 컴포넌트로 분리를 했으면 props로 받아와야하는 부분을 변경
-function ItemCol(props) {
+function ItemCol(props)
+{
   // 사용하는 곳에서 결정해줘야하는 부분(그때그때 바뀌어야하는 부분)만 props 처리
-  return (
+  return(
     <>
       <Col>
-        <img src={props.img} width={'200px'} height={'200px'} />
+        <img src={props.img} width={'300px'} height={'300px'}/>
         <h4>{props.data.title}</h4>
-        <p>{props.data.price}원</p>
+        <p>{props.data.price} 원</p>
       </Col>
+    </>
+  )
+}
+
+// 어바웃페이지의 컴포넌트
+function AboutPage()
+{
+  return(
+    <>
+      <div>
+        <h4>어바웃 페이지</h4>
+        {/* Outlet으로 중첩Route의 위치를 결정 */}
+        <Outlet/>
+      </div>
     </>
   )
 }
