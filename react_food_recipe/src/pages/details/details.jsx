b35a1e1a153aea708dfa8ab436d96f3b1d8922b7
ljsoo01 <ljsu1225@gmail.com>
2024-05-12 13:02:11 +0900

    5/12

---
+++ react_food_recipe/src/pages/details/details.jsx
@@ -0,0 +1,8 @@
+import './detail.css';
+
+export default function Details() {
+
+  return (
+    <div>디테일 컴포넌트</div>
+  )
+}
---
+++ react_food_recipe/src/pages/favorites/favorites.jsx
@@ -0,0 +1,8 @@
+import './favorites.css';
+
+export default function Favorites() {
+
+  return (
+    <div>페이보릿 컴포넌트</div>
+  )
+}
